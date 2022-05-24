7a71dd614167314ac1ff886de8330290cbf737dd
zhulibo <zhulibochn@gmail.com>
2022-05-24 14:34:08 +0800

    gamersky浏览体验优化

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         张鑫旭博客去广告、游民星空H5新闻页优化、知乎文章详情页隐藏标题
+// @name         张鑫旭博客去广告、游民星空H5新闻页浏览体验优化、知乎文章详情页隐藏标题
 // @namespace    http://tampermonkey.net/
 // @version      1.00
-// @description  张鑫旭博客去广告、游民星空H5新闻页优化、知乎文章详情页隐藏标题，问题反馈773520054@qq.com
+// @description  优化一些网站的浏览体验，问题反馈773520054@qq.com
 // @author       zhu
 // @match        *://www.zhangxinxu.com/*
 // @match        *://wap.gamersky.com/news/*
@@ -58,9 +58,52 @@ document.addEventListener('DOMContentLoaded',function(){
   }
 
   else if(url.match('://wap.gamersky.com/news/')){ // 游民星空H5
-    // 新闻详情页文章自动展开
-    let node = document.querySelector('#gsAreaContextOpen')
-    if(node) node.click()
+    {
+      // 去除一些新闻页面元素
+      let items = [
+        '#gsTgWapTop', // 列表页、详情页顶部广告
+        '.ymw-header2018', // 详情页head
+        '.ymw-header2019', // 列表页顶部引导下载
+        '.ymw-header2021', // 详情页顶部引导下载
+        '.gsTgWapConBdshareTopBox', // 详情页打开游民APP，查看更多精彩内容
+        '.gameCard', // 详情页gameCard
+        '.ymw-rel-list', // 详情页app精彩推荐、相关内容
+        '.ymw-hot-h5-game', // 详情页热门h5手游
+        '.ymw-footer', // 详情页footer
+      ]
+      for (let i = 0; i < items.length; i++) {
+        let node = document.querySelectorAll(items[i])
+        if(node && node.length > 0) {
+          for (let i = 0; i < node.length; i++) {
+            node[i].remove()
+          }
+        }
+      }
+    }
+    // // todo 未知原因 click事件不生效
+    // {
+    //   // 触底自动加载更多
+    //   window.addEventListener('scroll', loadData)
+    //   function loadData() {
+    //     let clientHeight = document.documentElement.clientHeight
+    //     let scrollHeight = document.documentElement.scrollHeight
+    //     let scrollTop = document.documentElement.scrollTop
+    //     if (scrollTop + clientHeight >= scrollHeight) {
+    //       let node = document.querySelector('.ymw-more')
+    //       if(node) node.click()
+    //     }
+    //   }
+    // }
+    {
+      // 新闻详情页文章自动展开
+      let node = document.querySelector('#gsAreaContextOpen')
+      if(node) node.click()
+    }
+    {
+      // 新闻详情页修改内容上边距
+      let node = document.querySelector('.ymwContxt')
+      if(node) node.style.paddingTop = '.4rem'
+    }
   }
 
 })
