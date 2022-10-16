9e9a4996d3ac1a79c1bb6f6d15a679646d3bb1e4
zhulibo <zhulibochn@gmail.com>
2022-10-16 16:09:20 +0800

    close zhihu banner

--- src/sites/zhihu.js
+++ src/sites/zhihu.js
@@ -2,12 +2,25 @@ export default function () {
   // 网页加载完毕
   window.addEventListener('load',function(){
     if (location.href.match('://www.zhihu.com/')) {
-      // 文章详情页隐藏顶部标题
-      let node = document.querySelector('.PageHeader')
-      if (node) {
-        console.log('已触发 ' + '.PageHeader')
-        node.style.display = 'none'
+
+      {
+        // 文章详情页隐藏顶部标题
+        let node = document.querySelector('.PageHeader')
+        if (node) {
+          console.log('已触发 ' + '.PageHeader')
+          node.style.display = 'none'
+        }
       }
+
+      {
+        // 关闭顶部banner
+        let node = document.querySelector('.Topstory>div:first-child')
+        if (node) {
+          console.log('已触发 ' + '.Topstory>div:first-child')
+          node.style.display = 'none'
+        }
+      }
+
     }
     // 解除站内拦截
     if (location.href.match('://link.zhihu.com/')) {
