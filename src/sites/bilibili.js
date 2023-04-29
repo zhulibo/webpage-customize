8ef8ec861c9e43afec55711b06f450e62bb4bda1
zhulibo <zhulibochn@gmail.com>
2023-04-29 15:32:19 +0800

    bili

--- src/sites/bilibili.js
+++ src/sites/bilibili.js
@@ -4,14 +4,14 @@ export default function () {
   document.addEventListener('DOMContentLoaded',function(){
     if(location.href.match('://www.bilibili.com/')){
 
-      // {
-      //   // 关闭adblock插件检测提示窗口
-      //   let node = document.querySelector('.adblock-tips')
-      //   if (node) {
-      //     console.log('已触发 ' + '.adblock-tips')
-      //     node.remove()
-      //   }
-      // }
+      {
+        // 关闭adblock插件检测提示窗口
+        let node = document.querySelector('.adblock-tips')
+        if (node) {
+          console.log('已触发 ' + '.adblock-tips')
+          node.remove()
+        }
+      }
 
     }
   })
