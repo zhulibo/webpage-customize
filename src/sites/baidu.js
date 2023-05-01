51f123cc3b33d07675042dfe76db12ce71030177
zhulibo <zhulibochn@gmail.com>
2023-05-01 21:49:49 +0800

    rm baidu ad

--- src/sites/baidu.js
+++ src/sites/baidu.js
@@ -3,12 +3,16 @@ import {addStyle} from "js-fragment";
 
 export default function () {
   if(location.href.match('://tieba.baidu.com/')) {
-    window.addEventListener('load',function(){
+    document.addEventListener('DOMContentLoaded',function(){
       {
-        // 关闭列表中的广告
+        // 隐藏帖子列表中的广告
         addStyle('#thread_list > div{display: none}')
-        // 关闭侧边广告
-        addStyle('.tbui_aside_float_bar + div, #aside-ad, #fc-wrap{display: none}')
+        // 隐藏右侧侧广告
+        addStyle('#aside-ad{display: none}')
+        // 隐藏左侧广告
+        addStyle('body > div:has( > .label_text){display: none}')
+        // 隐藏评论中的广告
+        addStyle('#j_p_postlist > div[data-field="{}"]{display: none}')
       }
 
     })
